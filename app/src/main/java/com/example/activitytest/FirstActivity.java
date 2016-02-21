39f2f4cc1788396d0cf6abbc978143ecebe044f9
SiegfriedLee47 <SiegfriedLee47@gmail.com>
2016-02-20 17:03:50 -0800

    2.2.4 隐藏标题栏

--- app/src/main/java/com/example/activitytest/FirstActivity.java
+++ app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -2,6 +2,7 @@ package com.example.activitytest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
 /**
  * Created by Siegfried on 2/20/2016.
@@ -10,6 +11,7 @@ public class FirstActivity extends Activity {
     @Override
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.first_layout);
     }
 }
