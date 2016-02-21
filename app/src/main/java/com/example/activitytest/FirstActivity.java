f84b000c22b9680f59dcabdf890a1e1ea4ac5285
SiegfriedLee47 <SiegfriedLee47@gmail.com>
2016-02-20 17:15:42 -0800

    2.2.5 在活动中使用Toast

--- app/src/main/java/com/example/activitytest/FirstActivity.java
+++ app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -1,8 +1,12 @@
 package com.example.activitytest;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by Siegfried on 2/20/2016.
@@ -13,5 +17,12 @@ public class FirstActivity extends Activity {
         super.onCreate(saveInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.first_layout);
+        Button button1 = (Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FirstActivity.this, "You Clicked Button 1", Toast.LENGTH_SHORT);
+            }
+        });
     }
 }
