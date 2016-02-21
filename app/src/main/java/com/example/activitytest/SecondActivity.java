3b8767983a8933e2314717c10d4fbd04ec1e65c3
SiegfriedLee47 <SiegfriedLee47@gmail.com>
2016-02-20 17:43:28 -0800

    2.3.1 使用Intent在活动中穿梭

---
+++ app/src/main/java/com/example/activitytest/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.example.activitytest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+/**
+ * Created by Siegfried on 2/20/2016.
+ */
+public class SecondActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.second_layout);
+    }
+}
