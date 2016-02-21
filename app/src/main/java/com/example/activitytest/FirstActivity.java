792d57c911555ee97c488cae430e9ef2c8dc8377
SiegfriedLee47 <SiegfriedLee47@gmail.com>
2016-02-20 17:35:47 -0800

    2.2.7 销毁一个活动

--- app/src/main/java/com/example/activitytest/FirstActivity.java
+++ app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -24,7 +24,7 @@ public class FirstActivity extends Activity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(FirstActivity.this, "You Clicked Button 1", Toast.LENGTH_SHORT).show();
+                finish();
             }
         });
     }
