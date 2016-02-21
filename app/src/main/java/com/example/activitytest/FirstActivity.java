72960477f3a1d9f390fe1379f9eb58abc2138e81
SiegfriedLee47 <SiegfriedLee47@gmail.com>
2016-02-20 19:20:45 -0800

    2.3.5 返回数据给上一个活动

--- app/src/main/java/com/example/activitytest/FirstActivity.java
+++ app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -25,12 +26,23 @@ public class FirstActivity extends Activity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String data = "Hello SecondActivity";
                 Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
-                intent.putExtra("extra_data", data);
-                startActivity(intent);
+                startActivityForResult(intent, 1);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    String returnedData = data.getStringExtra("data_return");
+                    Log.d("FirstActivity", returnedData);
+                }
+                break;
+            default:
+        }
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/com/example/activitytest/SecondActivity.java
+++ app/src/main/java/com/example/activitytest/SecondActivity.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 /**
  * Created by Siegfried on 2/20/2016.
@@ -15,8 +17,24 @@ public class SecondActivity extends Activity {
         super.onCreate(saveInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.second_layout);
-        Intent intent = getIntent();
-        String data = intent.getStringExtra("extra_data");
-        Log.d("SecondActivity", data);
+
+        Button button2 = (Button) findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("data_return", "Hello FirstActivity");
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        intent.putExtra("data_return", "Hello FirstActivity");
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
